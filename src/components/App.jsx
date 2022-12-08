88f84538f5a20f6de31072c0382ba6486a2e18e0
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-12-08 19:06:56 +0200

    connected searchbar with App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,33 +6,29 @@ import Searchbar from "./Searchbar/Searchbar";
 
 export default class App extends Component {
   state = {
+    searchName: '',
     image: null,
   }
    async componentDidMount() {
     await fetch('https://pixabay.com/api/?q=cat&page=1&key=28720978-48527d1c9d73f1bfd555e68c2&image_type=photo&orientation=horizontal&per_page=12')
       .then(res => res.json()).then(json => {
         this.setState({ image: json.hits, })
-        console.log("log в фетче", this.state.image)
+        // console.log("log в фетче", this.state.searchName)
         
       });
-    //  this.setState({ image: res.hits, })
-    //  console.log(this.state.image[0]);
+    
+  }  
+  handleSubmit = (data) => {
+    this.setState ({
+      searchName:data,
+      })
   }
-   
-  // componentDidMount() {
-  //   fetch('https://pixabay.com/api/?q=cat&page=1&key=28720978-48527d1c9d73f1bfd555e68c2&image_type=photo&orientation=horizontal&per_page=12')
-  //     .then(res => res.json()).then(picture => this.setState({ image: picture.webformatURL }));
-  //   console.log(this.state.image);
-  // }
-  
 
   render() {
     return (
       <div>
-        <Searchbar />
-        {this.state.image && <li class="gallery-item">
-      <img src="https://pixabay.com/get/gdfc56165ee2783cab9747a51ea2c8952509341d9b9c328c479d5cc1e70732fe800da16ad55c5e217e431ea4b742f864291e3d04f2dc7177876a69daf7a53d9c4_1280.jpg" alt="Rjifrf" />
-    </li>
+        <Searchbar onSubmit={ this.handleSubmit} />
+        {this.state.image && <h1 >{this.state.searchName}</h1>
         }
       
       </div>
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import './Searchbar.css'
 
-const Searchbar = () => (
-    <header class="searchbar">
-        <form class="form">
-            <button type="submit" class="button">
-                <span class="button-label">Search</span>
-            </button>
+class Searchbar extends Component { 
+    state = {
+        searshName: '',
+    }
+    handleInput = e => {
+        this.setState({ searshName: e.currentTarget.value.toLowerCase() })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.searshName);
+        console.log("searshName при сабмите", this.state.searshName)
+    }
+    render() {
+        console.log("searshName в рендері", this.state.searshName);
+    return (
+        <header class="searchbar">
+            <form onSubmit={this.handleSubmit }class="form">
+                <button type="submit" class="button">
+                    <span class="button-label">Search</span>
+                </button>
 
-            <input
-            class="input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-            />
-        </form>
-    </header>
-)
-
+                <input
+                    onChange={this.handleInput}
+                    class="input"
+                    type="text"
+                    value={this.state.searshName}
+                    autocomplete="off"
+                    autofocus
+                    placeholder="Search images and photos"
+                />
+            </form>
+        </header>
+    )
+}
+}
 export default Searchbar;
