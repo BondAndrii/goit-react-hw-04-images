280e1184569a3050aa953078e3791e138533e438
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-12-15 11:22:39 +0200

    back main state in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,90 @@
 import { Component } from "react";
 // import { ToastContainer } from "react-toastify";
 import Searchbar from "./Searchbar/Searchbar";
-import './styles.css'
+import Button from "./Button";
 import ImageGallery from "./ImageGallery";
+import Modal from "./Modal";
+import './styles.css'
 
 export default class App extends Component {
   state = {
-    searchName: '',   
-  }    
+    searchName: '',
+    images: null,
+    forModal: {},
+    page: 1,
+    loading: false,
+    showModal: false,
+    showBtn: false,
+    status: 'idle',
+  } 
+  async componentDidUpdate(prevProps, prevState) {
+        const { searchName, page, showBtn} = this.state;
+        const key = '28720978-48527d1c9d73f1bfd555e68c2';    
+    // console.log("in Gallery", this.props.searchName, this.state);
+        if (prevState.searchName !== searchName || prevState.page !== page)
+            try {
+                this.setState({ status: 'pending' })// Взводим умову для загрузки лоадера
+                const fetchRespons = await fetch(`https://pixabay.com/api/?q=${searchName}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`);
+                const imagesData = await fetchRespons.json();
+                const imagesList = imagesData.hits;
+                if (imagesList.length === 0) {
+                    this.setState({
+                        status: 'rejected'
+                    })                    
+                }
+                else {
+                    this.setState({
+                        images: imagesList,
+                        status: 'resolved',
+                    })
+              }
+              if (imagesList.length === 12) {
+                this.setState({
+                  showBtn: true,
+                })
+              }
+              else {
+                this.setState({
+                  showBtn: false,
+                })
+              }
+              console.log("in fetch", imagesData.totalHits) 
+            
+            } catch (error) {
+                alert(error);
+            }
+        }
   handleSubmit = (searchName) => {    
     this.setState({
       searchName, // отримуємо ім'я пошукового слова з searchbar
     });    
   }
- 
+ handleButton = (prevState) => {
+    this.setState(prevState => ({
+      page: prevState.page +1,// при натисканні кнопки збільшуємо номер сторінки на 1
+    }))    
+  }
+  toggleModal = () => this.setState({
+    showModal: !this.state.showModal,
+  })
+  handleClickImg = forModal => {
+    // console.log("приход с итема", forModal);
+    this.setState({
+      forModal,
+      showModal:true,
+    })
+    
+    console.log("after click", this.state);
+    
+  }
   render() {
-    const { searchName} = this.state;
+    const { images, showModal, forModal, status, showBtn} = this.state;
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery searchName={searchName} onClick={this.handleClickImg} /> 
+        <ImageGallery status={status} images={images} onClick={this.handleClickImg} />
+        {showBtn && <Button onClick={this.handleButton} /> }
+        {showModal && <Modal forRender={forModal} onClose={this.toggleModal} />}
       </div>
     );
   };
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,80 +1,31 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import ImageGalleryItem from "components/ImageGalleryItem";
-import Button from "components/Button";
 import { BallTriangle } from "react-loader-spinner";
-import Modal from "components/Modal";
 import "./ImageGallery.css"
 
 class ImageGallery extends Component {
     state = {
-        searchName: '',
-        page: 1,
-        images: null,
-        loading: false,
-        forModal: {},
-        status: 'idle',
-        showModal: false,        
+        // searchName: '',        
+        // images: null,
+        
+        // forModal: {},
+        // status: 'idle',
+                
     }
     // 'idle'
     // 'pending'
     // 'resolved'
     // 'rejected'
     // 'modal'
-    async componentDidUpdate(prevProps, prevState) {
-        const { searchName, page, } = this.state;
-        const key = '28720978-48527d1c9d73f1bfd555e68c2';
-        
-        
-        this.setState({
-            searchName: this.props.searchName,
-        })
-        // console.log("in Gallery", this.props.searchName, this.state);
-        if (prevState.searchName !== searchName || prevState.page !== page)
-            try {
-                this.setState({ status: 'pending' })// Взводим умову для загрузки лоадера
-                const fetchRespons = await fetch(`https://pixabay.com/api/?q=${searchName}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`);
-                const imagesData = await fetchRespons.json();
-                const imagesList = imagesData.hits;
-                if (imagesList.length === 0) {
-                    this.setState({
-                        status: 'rejected'
-                    })
-                    
-                }
-                else {
-                    this.setState({
-                        images: imagesList,
-                        status: 'resolved',
-                    })
-                }
-                // console.log("in fetch", imagesList .length) 
-            
-            } catch (error) {
-                alert(error);
-            }
-        }
     
-    handleButton = (prevState) => {
-    this.setState(prevState => ({
-      page: prevState.page +1,// при натисканні кнопки збільшуємо номер сторінки на 1
-    }))    
-    }
-    handleClickImg = forModal => {
-    // console.log("приход с итема", forModal);
-    this.setState({
-      forModal,
-      showModal:true,
-    })
     
-    console.log("after click", this.state);
     
-  }
-    toggleModal = () => this.setState({
-    showModal: !this.state.showModal,
-  }) 
+   
+  
     render() {
-        const { images, status, showModal, forModal } = this.state;
+        // const { showModal, forModal } = this.state;
+        const { status, images, onClick } = this.props;
         if (status === 'idle') {
             return <h2>Введіть, щоб ви хотіли побачити...</h2>
         }
@@ -94,16 +45,14 @@ class ImageGallery extends Component {
             return <h2>Нажаль, нічого не знайшли</h2>
         }
         if (status === 'resolved'){
-            return (
-            <div className="Container">
+            return (            
                 <ul className="ImageGallery">                
                     {images.map(image => (
-                        <ImageGalleryItem key={image.id} item={image} onClick={this.handleClickImg} />
-                    ))}                    
-                    </ul>
-                    <Button onClick={this.handleButton} /> 
-                    {showModal && <Modal forRender={forModal} onClose={this.toggleModal} />}
-            </div>)
+                        <ImageGalleryItem key={image.id} item={image} onClick={onClick} />
+                    ))}                   
+                </ul>
+                    
+            )
     }}
 }
 
